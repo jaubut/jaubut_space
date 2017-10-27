fdcad12f3a01a5430e8cc20e59624b56509028c7
Jeremie Aubut <jeremie.aubut@gmail.com>
2017-10-27 15:26:26 -0400

    testing login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,30 @@
 <template>
   <div id='Login'>
+    <button @click="handleLogin">Login</button>
     <h2>Login page</h2>
+    <p>Login as {{ user }}</p>
   </div>
 </template>
 <script>
+import netlifyIdentity from 'netlify-identity-widget'
+
+netlifyIdentity.init()
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      user: ''
+    }
+  },
+  methods: {
+    handleLogin () {
+      netlifyIdentity.open()
+    },
+    handleUser () {
+      this.user = netlifyIdentity.currentUser()
+    }
+  }
 }
 </script>
 <style lang='sass' scoped>
