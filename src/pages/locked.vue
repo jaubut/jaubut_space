b5d27c058b310485ead3853e15f1653061cb733a
Jeremie Aubut <jeremie.aubut@gmail.com>
2017-10-27 15:16:35 -0400

    testing routes locked.. without login

--- src/pages/locked.vue
+++ src/pages/locked.vue
@@ -1,30 +1,11 @@
 <template>
   <div id='locked'>
-    <button @click="handleLogin">Log in with netlify</button>
     <h2>Locked page</h2>
-    <p>Login as {{ user }}</p>
   </div>
 </template>
 <script>
-import netlifyIdentity from 'netlify-identity-widget'
-
-netlifyIdentity.init()
-
 export default {
-  name: 'locked',
-  data () {
-    return {
-      user: ''
-    }
-  },
-  methods: {
-    handleLogin () {
-      netlifyIdentity.open()
-    },
-    handleUser () {
-      this.user = netlifyIdentity.currentUser()
-    }
-  }
+  name: 'locked'
 }
 </script>
 <style lang='sass' scoped>
